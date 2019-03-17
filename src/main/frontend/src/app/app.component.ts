e210bf4e7c38e60cdae6d2c49bbb825bd7828757
DennisD2 <dd2@dingeldein-online.de>
2019-03-17 17:39:39 +0100

    Small changes; added "ADD" html as a draft.

--- src/main/frontend/src/app/app.component.ts
+++ src/main/frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { HelloService } from './services/hello.service';
 import { XDocument } from './model/XDocument';
+// import {XDocMock} from "./services/XDocListMock";
 
 @Component({
   selector: 'app-root',
@@ -23,4 +24,9 @@ export class AppComponent {
           self.xdocs = docs;
       });
   }
+
+    public save(): void {
+      console.log("save!")
+        return ;
+    }
 }
