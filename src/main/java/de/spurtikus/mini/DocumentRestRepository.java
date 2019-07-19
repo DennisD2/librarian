008ce696d5add507473dfef49e3cea80fa018dac
DennisD2 <info@spurtikus.de>
2019-07-19 10:48:47 +0200

    Increased default page site Replaced PagingAndSortingRepository with CrudRepository.

--- src/main/java/de/spurtikus/mini/DocumentRestRepository.java
+++ src/main/java/de/spurtikus/mini/DocumentRestRepository.java
@@ -1,13 +1,12 @@
 package de.spurtikus.mini;
 
 import de.spurtikus.mini.data.model.Document;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 //@CrossOrigin
-public interface DocumentRestRepository extends PagingAndSortingRepository<Document, Long> {
+public interface DocumentRestRepository extends CrudRepository<Document, Long> {
     // Additional method here
     //List<Document> findByTitle(@Param("title") String title);
     //Document removeCategory(@Param("id") Long id, @Param("category") String category);
