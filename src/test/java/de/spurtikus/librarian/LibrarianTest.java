ab147cdfe1dffd6ef2aebc4c0ab00c855bb22191
DennisD2 <info@spurtikus.de>
2019-08-07 20:07:37 +0200

    Extended tests.

--- src/test/java/de/spurtikus/librarian/LibrarianTest.java
+++ src/test/java/de/spurtikus/librarian/LibrarianTest.java
@@ -9,13 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -24,28 +21,20 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LibrarianTest {
 
+	String baseDirectory = "/home/dennis/doclib/";
+
 	@Autowired
 	DocumentRepository documentRepository;
 
-	//String basePath = "/home/dennis/";
-
-	private FileCollector collectFiles(String baseDirectory) throws IOException {
-		Path path = Paths.get(baseDirectory);
-		FileCollector fileVisitor = new FileCollector();
-		Files.walkFileTree(path, fileVisitor);
-		return fileVisitor;
-	}
+	StoreAnalyzer storeAnalyzer = new StoreAnalyzer();
 
 	@Test
 	public void testDirRead() throws IOException {
-		String baseDirectory = "/home/dennis/doclib/";
-
-		FileCollector fileVisitor = collectFiles(baseDirectory);
+		FileCollector fileVisitor = storeAnalyzer.collectFiles(baseDirectory);
 		for (String file : fileVisitor.getFiles()) {
 			file = file.replace(baseDirectory, "");
 			System.out.println("FILE: " + file);
 		}
-
 	}
 
 	@Test
@@ -65,76 +54,30 @@ public class LibrarianTest {
 		docs.forEach(doc -> docInDB.add(doc));
 
 		// get all files in filesystem
-		String baseDirectory = "/home/dennis/doclib/";
-		FileCollector fileVisitor = collectFiles(baseDirectory);
+		FileCollector fileVisitor = storeAnalyzer.collectFiles(baseDirectory);
 		List<String> docInFS = fileVisitor.getFiles();
 
 		System.out.println("Size DB: " + docInDB.size());
 		System.out.println("Size FS: " + docInFS.size());
 
 		// Files in FS but not in DB
-		filesInFSButNotInDB(docInDB, baseDirectory, docInFS);
+		List<String> fsOrphans = storeAnalyzer.filesInFSButNotInDB(docInDB, baseDirectory, docInFS);
+		System.out.println("-----------------\nFS Orphans: Number of objects 'in FS but not in DB': " + fsOrphans.size());
+		fsOrphans.forEach(doc -> System.out.println("In FS but not in DB: " + doc));
 
 		// Files in DB but not in FS
-		filesInDBButNotInFS(docInDB, baseDirectory, docInFS);
-
+		List<String> dbOrphans = storeAnalyzer.filesInDBButNotInFS(docInDB, baseDirectory, docInFS);
+		System.out.println("-----------------\nDB Orphans: Number of objects 'in DB but not in FS': " + dbOrphans.size());
+		dbOrphans.forEach(doc -> System.out.println("In DB but not in FS: " + doc));
 	}
 
-	private boolean isIgnoredLocation(String location) {
-		return location.startsWith("rsync-logs/") || location.startsWith("bin/") || location.startsWith("new/");
-	}
-
-	private void filesInFSButNotInDB(ArrayList<Document> docInDB, String baseDirectory, List<String> docInFS) {
-		List<String> docInDBStreamed = docInDB.stream().map(d -> d.getLocation()).map(d -> reEscape(d)).collect(Collectors.toList());
-		List<String> docInFSStreamed = docInFS.stream().map(d -> d.replace(baseDirectory, "")).map(d -> reEscape(d)).collect(Collectors.toList());
-		System.out.println("Size Streamed: " + docInDBStreamed.size());
-
-		List<String> docToRemoveFromFSList = new ArrayList<>();
-		docInDBStreamed.forEach(location -> {
-			//String newLocation = reEscape(location);
-			//System.out.println("Check: " + location );
-			if (docInFSStreamed.contains(location)) {
-				docToRemoveFromFSList.add(location);
-				//System.out.println("REMOVED: " + location);
-			}
+	@Test
+	public void testDoublettes() throws IOException, NoSuchAlgorithmException {
+		Map<String, String> doublettes = storeAnalyzer.getDoublettes(baseDirectory);
+		doublettes.forEach((doc1,doc2) -> {
+			System.out.println("Doublette candidate: " + doc1.replace(baseDirectory,"")
+				+ "(compared with: " + doc2.replace(baseDirectory,"") + ")");
 		});
-		docInFSStreamed.forEach(location -> {
-			if (isIgnoredLocation(location)) {
-				docToRemoveFromFSList.add(location);
-			}
-		});
-		docToRemoveFromFSList.forEach(doc -> docInFSStreamed.remove(doc));
-		// now docInFSStreamed contains all docs that are not in DB!
-		System.out.println("-----------------\nFS Orphans: Number of objects 'in FS but not in DB': " + docInFSStreamed.size());
-		docInFSStreamed.forEach(doc -> System.out.println("In FS but not in DB: " + doc));
-	}
-
-	private void filesInDBButNotInFS(ArrayList<Document> docInDB, String baseDirectory, List<String> docInFS) {
-		List<String> docInDBStreamed = docInDB.stream().map(d -> d.getLocation()).map(d -> reEscape(d)).collect(Collectors.toList());
-		List<String> docInFSStreamed = docInFS.stream().map(d -> d.replace(baseDirectory, "")).map(d -> reEscape(d)).collect(Collectors.toList());
-		System.out.println("Size Streamed: " + docInDBStreamed.size());
-
-		List<String> docToRemoveFromDBList = new ArrayList<>();
-		docInFSStreamed.forEach(location -> {
-			//System.out.println("Check: " + location );
-			//String newLocation = reEscape(location);
-			if (docInDBStreamed.contains(location)) {
-				docToRemoveFromDBList.add(location);
-				//System.out.println("REMOVED: " + newLocation);
-			}
-		});
-
-		docToRemoveFromDBList.forEach(doc -> docInDBStreamed.remove(doc));
-		// now docInDBStreamed contains all docs that are not in FS!
-		System.out.println("-----------------\nDB Orphans: Number of objects 'in DB but not in FS': " + docInDBStreamed.size());
-		docInDBStreamed.forEach(doc -> System.out.println("In DB but not in FS: " + doc));
-	}
-
-	private String reEscape(String location) {
-		if (location.contains("%")) {
-			return URLDecoder.decode(location);
-		}
-		return location;
 	}
 
 }
---
+++ src/test/java/de/spurtikus/librarian/StoreAnalyzer.java
@@ -0,0 +1,114 @@
+package de.spurtikus.librarian;
+
+import de.spurtikus.librarian.data.model.Document;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StoreAnalyzer {
+
+    public FileCollector collectFiles(String baseDirectory) throws IOException {
+        Path path = Paths.get(baseDirectory);
+        FileCollector fileVisitor = new FileCollector();
+        Files.walkFileTree(path, fileVisitor);
+        return fileVisitor;
+    }
+
+    private boolean isIgnoredLocation(String location) {
+        return location.startsWith("rsync-logs/") || location.startsWith("bin/") || location.startsWith("new/");
+    }
+
+    public List<String> filesInFSButNotInDB(ArrayList<Document> docInDB, String baseDirectory, List<String> docInFS) {
+        List<String> docInDBStreamed = docInDB.stream().map(d -> d.getLocation()).map(d -> reEscape(d)).collect(Collectors.toList());
+        List<String> docInFSStreamed = docInFS.stream().map(d -> d.replace(baseDirectory, "")).map(d -> reEscape(d)).collect(Collectors.toList());
+
+        List<String> docToRemoveFromFSList = new ArrayList<>();
+        docInDBStreamed.forEach(location -> {
+            //String newLocation = reEscape(location);
+            //System.out.println("Check: " + location );
+            if (docInFSStreamed.contains(location)) {
+                docToRemoveFromFSList.add(location);
+                //System.out.println("REMOVED: " + location);
+            }
+        });
+        docInFSStreamed.forEach(location -> {
+            if (isIgnoredLocation(location)) {
+                docToRemoveFromFSList.add(location);
+            }
+        });
+        docToRemoveFromFSList.forEach(doc -> docInFSStreamed.remove(doc));
+        // now docInFSStreamed contains all docs that are not in DB!
+        return docInFSStreamed;
+    }
+
+    public List<String> filesInDBButNotInFS(ArrayList<Document> docInDB, String baseDirectory, List<String> docInFS) {
+        List<String> docInDBStreamed = docInDB.stream().map(d -> d.getLocation()).map(d -> reEscape(d)).collect(Collectors.toList());
+        List<String> docInFSStreamed = docInFS.stream().map(d -> d.replace(baseDirectory, "")).map(d -> reEscape(d)).collect(Collectors.toList());
+
+        List<String> docToRemoveFromDBList = new ArrayList<>();
+        docInFSStreamed.forEach(location -> {
+            //System.out.println("Check: " + location );
+            //String newLocation = reEscape(location);
+            if (docInDBStreamed.contains(location)) {
+                docToRemoveFromDBList.add(location);
+                //System.out.println("REMOVED: " + newLocation);
+            }
+        });
+
+        docToRemoveFromDBList.forEach(doc -> docInDBStreamed.remove(doc));
+        // now docInDBStreamed contains all docs that are not in FS!
+        return docInDBStreamed;
+    }
+
+    public String reEscape(String location) {
+        if (location.contains("%")) {
+            return URLDecoder.decode(location);
+        }
+        return location;
+    }
+
+    public Map<String, String> getDoublettes(String baseDirectory) throws IOException {
+        FileCollector fileVisitor = collectFiles(baseDirectory);
+        List<String> docInFS = fileVisitor.getFiles();
+
+        List<String> docInFSStreamed = docInFS.stream().map(d -> reEscape(d)).collect(Collectors.toList());
+
+        Map<Long, String> fileWithHash = new HashMap<>();
+        Map<String, String> doublettes = new HashMap<>();
+        //MessageDigest md = MessageDigest.getInstance("MD5");
+        docInFSStreamed.forEach(doc -> {
+            File f = new File(doc);
+            long length = f.length();
+            if (fileWithHash.containsKey(length)) {
+                doublettes.put(doc.replace(baseDirectory,""),
+                        fileWithHash.get(length).replace(baseDirectory,""));
+            } else {
+                fileWithHash.put(f.length(), doc);
+            }
+        });
+        return doublettes;
+    }
+
+	/*private static byte[] checksum(String filepath, MessageDigest md) {
+		try (DigestInputStream dis = new DigestInputStream(new FileInputStream(filepath), md)) {
+			while (dis.read() != -1);
+			md = dis.getMessageDigest();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return md.digest();
+	}*/
+}
