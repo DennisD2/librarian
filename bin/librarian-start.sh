5c17eb9dacc2f34954420646ad8c40a6a17880a6
DennisD2 <info@spurtikus.de>
2020-02-02 20:01:59 +0100

    Fixed service file

--- bin/librarian-start.sh
+++ bin/librarian-start.sh
@@ -2,5 +2,11 @@
 librarian_home=/home/pi/librarian
 
 cd $librarian_home
-/usr/bin/java -Dspring.config.location=$librarian_home/librarian-config.properties -jar $librarian_home/librarian-1.0.0-SNAPSHOT.jar > $librarian_home/librarian.log 2>&1 &
+
+# Start for standalone
+#/usr/bin/java -Dspring.config.location=$librarian_home/librarian-config.properties -jar $librarian_home/librarian-1.0.0-SNAPSHOT.jar > $librarian_home/librarian.log 2>&1 &
+
+# Start for systemd
+/usr/bin/java -Dspring.config.location=$librarian_home/librarian-config.properties -jar $librarian_home/librarian-1.0.0-SNAPSHOT.jar > $librarian_home/librarian.log
+
 echo "Librarian started"
